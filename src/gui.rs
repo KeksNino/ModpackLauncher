d44ff0af0f2a0ca6a3f7858019851827d3bf3ce7
KeksNino <KeksNino@proton.me>
2025-11-17 19:39:05 +0100

    added parsing for forge versions xml (incomplete)

--- src/gui.rs
+++ src/gui.rs
@@ -1,4 +1,5 @@
 use iced::widget::{button, column};
+use quick_xml::reader::Reader;
 
 pub fn run() -> iced::Result {
     iced::run("Mod Launcher", ModLauncher::update, ModLauncher::view)
@@ -13,9 +14,22 @@ enum Message {
 struct ModLauncher;
 
 impl ModLauncher {
-    fn update(&mut self, _message: Message) {}
+    fn update(&mut self, _message: Message) -> () {
+        match _message {
+            Message::DoSomething => (fetch_versions()),
+        }
+    }
 
     fn view(&self) -> iced::Element<Message> {
         column![button("Add Instance").on_press(Message::DoSomething),].into()
     }
+}
+
+fn fetch_versions() {
+    let xml = reqwest::get(
+        "https://maven.minecraftforge.net/net/minecraftforge/forge/maven-metadata.xml",
+    );
+    let mut reader = Reader::from_str(xml);
+    reader.config_mut().trim_text(true);
+    println!("{reader:?}");
 }
